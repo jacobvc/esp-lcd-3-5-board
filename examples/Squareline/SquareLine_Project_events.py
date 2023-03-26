281556c024b580ca0a3e9fa64e13d599f34b42bb
jacobvc <jacobvc@users.noreply.github.com>
2023-03-26 15:55:57 -0500

    Expanded example
    Example now demos I2C using BMP280 and SD Card, both via display button.

---
+++ examples/Squareline/SquareLine_Project_events.py
@@ -0,0 +1,8 @@
+
+def BtnSampleClicked(event_struct):
+    return
+
+
+def BtnLsClicked(event_struct):
+    return
+
--- examples/Squareline/main/main.c
+++ examples/Squareline/main/main.c
@@ -7,19 +7,23 @@
 #define TAG "MAIN"
 #define APP_DISP_DEFAULT_BRIGHTNESS 50
 
+/* SD Card test */
 #include <stdio.h>
 #include "dirent.h"
 #define MAX_DIR_BYTES 4096
 #define MOUNT_POINT "/sd"
 
-void sdcard_ls(sdmmc_card_t *card, const char *path)
+void BtnLsClicked(lv_event_t * e)
 {
+    const char *path = MOUNT_POINT;
+    esp_err_t err = 0;
+    lv_textarea_set_text(ui_txaFiles, "");
+    sdmmc_card_t *card = bsp_lcd_sdcard_mount(MOUNT_POINT, &err);
     if (card)
     {
         DIR *dir = opendir(path);
         struct dirent *de = readdir(dir);
         lv_label_set_text(ui_lblPath, path);
-        lv_textarea_set_text(ui_txaFiles, "");
         while (de)
         {
             lv_textarea_add_text(ui_txaFiles, de->d_name);
@@ -30,8 +34,58 @@ void sdcard_ls(sdmmc_card_t *card, const char *path)
             lv_textarea_add_text(ui_txaFiles, "\n");
             de = readdir(dir);
         }
+        bsp_lcd_sdcard_unmount(card, MOUNT_POINT);
+    }
+    else
+    {
+        lv_label_set_text(ui_lblPath, "Can't read SD Card");
     }
 }
+
+/* I2C test using BMP280 */
+#include "bmx280.h"
+
+bmx280_t* bmx280 = NULL;
+
+esp_err_t bmp280Init()
+{
+    bmx280 = bmx280_create(BSP_LCD_I2C_NUM);
+
+    if (!bmx280) { 
+        ESP_LOGE(TAG, "Could not create bmx280 driver.");
+        return -1;
+    }
+
+    esp_err_t err = bmx280_init(bmx280);
+    if (err == ESP_OK) {
+        bmx280_config_t bmx_cfg = BMX280_DEFAULT_CONFIG;
+        err = bmx280_configure(bmx280, &bmx_cfg);
+    }
+    if (err != ESP_OK) {
+        bmx280_close(bmx280);
+        bmx280 = NULL;
+    }
+    return err;
+}
+
+void BtnSampleClicked(lv_event_t * e)
+{
+    if (bmx280) {
+        ESP_ERROR_CHECK(bmx280_setMode(bmx280, BMX280_MODE_FORCE));
+        do {
+            vTaskDelay(pdMS_TO_TICKS(1));
+        } while(bmx280_isSampling(bmx280));
+
+        float temp = 0, pres = 0, hum = 0;
+        ESP_ERROR_CHECK(bmx280_readoutFloat(bmx280, &temp, &pres, &hum));
+        char buf[80];
+        sprintf(buf, "%f", pres);
+        lv_label_set_text(ui_lblPressure, buf);
+        sprintf(buf, "%f", temp);
+        lv_label_set_text(ui_lblTemp, buf);
+    }
+}
+
 
 void app_lvgl_display(void)
 {
@@ -56,15 +110,9 @@ void app_main(void)
 
     ESP_LOGI(TAG, "Initialization done.");
 
-    esp_err_t err;
-    sdmmc_card_t *card = bsp_lcd_sdcard_mount(MOUNT_POINT, &err);
-    if (card)
-    {
-        sdcard_ls(card, MOUNT_POINT);
-        bsp_lcd_sdcard_unmount(card, MOUNT_POINT);
-    }
-    else
-    {
-        lv_label_set_text(ui_lblPath, "Can't read SD Card");
+    bmp280Init();
+    if (!bmx280) {
+        lv_label_set_text(ui_lblPressure, "BMP280 NOT FOUND");
+        lv_label_set_text(ui_lblTemp, "BMP280 NOT FOUND");
     }
 }
